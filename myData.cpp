2c4a0eeb8940290d95c0b6d12ae72803fed51df7
Arturo Limon <arturolopezlimon@gmail.com>
2016-09-10 13:50:56 -0500

    first commit

---
+++ myData.cpp
@@ -0,0 +1,87 @@
+#include "myData.h"
+
+unsigned long startTimeStamp;
+
+bool IS_DEBUG_PRESSED(context * self)
+{
+    if (digitalRead(DEBUG_BUTTON))
+        return false;
+    mydelay(self, DEBOUNCE_DELAY);
+    if (digitalRead(DEBUG_BUTTON))
+        return false;
+    mydelay(self, DEBOUNCE_DELAY);
+    if (digitalRead(DEBUG_BUTTON))
+        return false;
+    return true;
+}
+// Acquire a data record.
+void acquireData(data_t * data, AssetTracker * t) {
+
+    data->time = micros() - startTimeStamp;
+
+    data->accelX = t->readX();
+    data->accelY = t->readY();
+    data->accelZ = t->readZ();
+
+    //t->updateGPS();
+
+    if(t->gpsFix())
+    {
+        data->lat = t->readLat();
+        data->lon = t->readLon();
+        data->vel = t->readVel();
+    }
+    else
+    {
+        data->lat = -1;
+        data->lon = -1;
+        data->vel = -1;
+    }
+}
+
+void flashDebugLed(context * self, unsigned int ntimes, unsigned int period)
+{
+    for(int i = 0; i < ntimes; i++)
+    {
+        digitalWrite(DEBUG_LED,HIGH);
+        mydelay(self, period/2);
+        digitalWrite(DEBUG_LED,LOW);
+        mydelay(self, period/2);
+    }
+}
+
+
+#define GPS_SAMP 500 //microseconds
+//period in millis
+void mydelay(context * self, unsigned int period)
+{
+    unsigned int iterNum = (period * 1000)/GPS_SAMP;
+    for(int i = 0; i < iterNum; i++)
+    {
+        self->t->updateGPS();
+        delayMicroseconds(GPS_SAMP);
+    }
+}
+// Print a data record.
+void printData(Print* pr, data_t* data) {
+    //pr->printf("%lu %u %.6f %.6f\n\r",data->time, data->accel, data->lat, data->lon);
+
+  pr->print(data->time);
+
+    pr->write(' ');
+    pr->print(data->accelX);
+    pr->write(' ');
+    pr->print(data->accelY);
+    pr->write(' ');
+    pr->print(data->accelZ);
+
+    pr->write(' ');
+  pr->printf("%.6f",data->lat);
+    pr->write(' ');
+  pr->printf("%.6f",data->lon);
+      pr->write(' ');
+  pr->printf("%.2f",data->vel);
+  pr->println();
+
+
+}
---
+++ myData.h
@@ -0,0 +1,45 @@
+#ifndef myData_h
+#define myData_h
+#include "myAssetTracker.h"
+#include "SdFat.h"
+
+
+#define GPS_RATE (unsigned long) 100
+#define GPS_FIX_ATTEMPTS    60
+#define GPS_BACKOFF         1000
+#define ADC_DIM             1
+#define DEBUG_BUTTON        C0
+#define DEBUG_LED           D7
+#define DEBOUNCE_DELAY      5
+#define LAT_OFFSET          (float) 2000
+#define LON_OFFSET          (float) 10300
+
+
+extern unsigned long startTimeStamp;
+
+struct context_st {
+    AssetTracker  * t;
+};
+
+typedef struct context_st context;
+
+
+struct data_st {
+  unsigned long time;
+  int accelX;
+  int accelY;
+  int accelZ;
+  float lat;
+  float lon;
+  float vel;
+
+};
+
+typedef struct data_st data_t;
+bool IS_DEBUG_PRESSED(context * self);
+void acquireData(data_t * data, AssetTracker * t);
+void printData(Print* pr, data_t* data);
+void flashDebugLed(context * self, unsigned int ntimes, unsigned int period);
+void mydelay(context * self, unsigned int period);
+
+#endif
