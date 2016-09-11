4c09fc3f41037ed7035cc619c703377dbf114817
Arturo Limon <arturolopezlimon@gmail.com>
2016-09-10 22:45:56 -0500

    Work continues on state.c . Several functions added. WIP

--- state.c
+++ state.c
@@ -1,5 +1,9 @@
-#define SLEEP_ST 0
-#define ALET1_ST 1
+#define  SLEEP_ST 0
+#define ALERT1_ST 1
+#define ALERT2_ST 2
+#define  SETUP_ST 3
+#define RECORD_ST 4
+#define   SHUT_ST 5
 
 /*******************************************************************************
   SLEEP STATE DEFINES
@@ -27,14 +31,15 @@
 
 typedef state unsigned char;
 
-struct stctx_st
+struct context_st
 {
   state thisState;
   state nxtState;
   unsigned int actCnt;
+  AssetTracker * t;
 };
 
-typedef stctx stctx_st;
+typedef context_st context;
 
 /*******************************************************************************
   NAME
@@ -42,7 +47,7 @@ typedef stctx stctx_st;
   DESCRIPTION
     Function for the sleep state in the main state machine
 *******************************************************************************/
-void sleepSt(stctx * ctx)
+void sleepSt(context * ctx)
 {
   ctx->nxtState = ctx->thisState;
 
@@ -66,7 +71,7 @@ void sleepSt(stctx * ctx)
     Function for the alert level 1 state in the main state machine. This state
     deals only with accelerometer activiy.
 *******************************************************************************/
-void alert1St(stctx * ctx)
+void alert1St(context * ctx)
 {
   ctx->nxtState = ctx->thisState;
 
@@ -90,7 +95,7 @@ void alert1St(stctx * ctx)
     Function for the alert level 2 state in the main state machine. This state
     deals with GPS activity.
 *******************************************************************************/
-void alert2St(stctx * ctx)
+void alert2St(context * ctx)
 {
   ctx->nxtState = ctx->thisState;
 
@@ -105,4 +110,125 @@ void alert2St(stctx * ctx)
   }
 
   gotoSleep(SLPTIM_ALER2ST);
+}
+
+/*******************************************************************************
+  NAME
+    recordSt - data recording state machine function
+  DESCRIPTION
+    Actual recording state function.
+*******************************************************************************/
+void recordSt(context * ctx)
+{
+  return; // NOOP for the time being TODO: do something funny here
+}
+
+/*******************************************************************************
+  NAME
+    shutSt - shutdown state machine function
+  DESCRIPTION
+    Shut everything down.
+*******************************************************************************/
+void shutSt(context * ctx)
+{
+  return; // NOOP for the time being TODO: do something funny here
+}
+
+/*******************************************************************************
+  NAME
+    advanceState - advance state machine
+  DESCRIPTION
+    Advance state machine. Prepare all context variables as wells
+    power profile.
+*******************************************************************************/
+void advanceState(context * ctx)
+{
+  updatePowerProfile(ctx);
+  resetContext(ctx);
+  ctx->thisState = ctx->nxtState;
+}
+
+/*******************************************************************************
+  NAME
+    resetContext - reset context
+  DESCRIPTION
+    Prepare global  context for the next state. Reset variables to null.
+*******************************************************************************/
+void resetContext(context * ctx)
+{
+  ctx->actCnt = 0;
+}
+
+/*******************************************************************************
+  NAME
+    updatePowerProfile - Update power profile
+  DESCRIPTION
+    This function turns ON/OFF modules depending on next state.
+*******************************************************************************/
+void updatePowerProfile(context * ctx)
+{
+  switch(ctx->nxtState)
+  {
+    case(SLEEP_ST):
+      ctx->t->accOn();
+      ctx->t->gpsOff();
+      break;
+    case(ALERT1_ST):
+      //ctx->t->accOn();
+      ctx->t->gpsOff();
+      break;
+    case(ALERT2_ST):
+      //ctx->t->accOn();
+      ctx->t->gpsOn();
+      break;
+    case(SETUP_ST):
+      //ctx->t->accOn();
+      ctx->t->gpsOn();
+      break;
+    case(RECORD_ST):
+      //ctx->t->accOn();
+      ctx->t->gpsOn();
+      break;
+    case(SHUT_ST):
+      //ctx->t->accOn();
+      ctx->t->gpsOff();
+      break;
+    default:
+      /*TODO: print some sort of error message if we end up here */
+      break;
+  }
+}
+
+/*******************************************************************************
+  NAME
+    runState - run state machine state
+  DESCRIPTION
+    Launch the corresponding function depending on current state.
+*******************************************************************************/
+void runState(context * ctx)
+{
+  switch(ctx->thisState)
+  {
+    case(SLEEP_ST):
+      sleepSt(ctx);
+      break;
+    case(ALERT1_ST):
+      alert1St(ctx);
+      break;
+    case(ALERT2_ST):
+      alert2St(ctx);
+      break;
+    case(SETUP_ST):
+      setupSt(ctx);
+      break;
+    case(REC_ST):
+      recordSt(ctx);
+      break;
+    case(SHUT_ST):
+      shutSt(ctx);
+      break;
+    default:
+      /*TODO: print some sort of error message if we end up here */
+      break;
+  }
 }
